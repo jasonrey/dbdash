064ddc753fb8208a166d3d7e2736d7052693e129
JSON object <jsonobject@outlook.com>
2017-11-13 20:43:58 +0800

    Update project migration

--- migrations/20171112182635_project.js
+++ migrations/20171112182635_project.js
@@ -2,7 +2,7 @@ exports.up = db => {
   return db.schema.createTable('project', t => {
     t.increments()
     t.string('name').notNullable()
-    t.string('secret').notNullable()
+    t.string('identifier').notNullable().index()
     t.integer('userId').notNullable().unsigned().references('id').inTable('user')
     t.timestamp('createdAt').defaultTo(db.fn.now())
     t.timestamp('updatedAt').defaultTo(db.fn.now())
