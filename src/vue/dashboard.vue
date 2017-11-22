2c1ae1568a7e67177f8a8b9f62efd02d4466ee37
JSON object <jsonobject@outlook.com>
2017-11-22 23:07:52 +0800

    Update frontend

--- src/vue/dashboard.vue
+++ src/vue/dashboard.vue
@@ -1,9 +1,29 @@
 <template lang="pug">
-p {{ dashboardId }}
+div
+  p(v-for="widget in widgets", :key="widget.id") {{ widget.id }}
 </template>
 
 <script>
+import api from '../library/api'
+
 export default {
-  props: ['dashboardId']
+  props: ['dashboardId'],
+  data () {
+    return {
+      dashboard: null,
+      widgets: []
+    }
+  },
+  created () {
+    api.get(`dashboard/${this.dashboardId}`)
+      .then(res => {
+        this.dashboard = res
+      })
+
+    api.get(`dashboard/${this.dashboardId}/widgets`)
+      .then(res => {
+        this.widgets = res
+      })
+  }
 }
 </script>
--- src/vue/navbar/user.vue
+++ src/vue/navbar/user.vue
@@ -1,9 +1,6 @@
 <template lang="pug">
-ul.navbar-nav.my-lg-0
-  li.nav-item.dropdown
-    button.btn.btn-link.btn-block.text-left.nav-link.dropdown-toggle.border-0(@click="showNav = !showNav")
-    .dropdown-menu.dropdown-menu-right(:class="{ show: showNav }")
-      button.btn.btn-link.btn-block.text-left.dropdown-item(@click="logout") Logout
+.navbar-nav.my-lg-0.text-right
+  button.btn.btn-link.nav-item.nav-link.text-right(@click="logout") Logout
 </template>
 
 <script>
