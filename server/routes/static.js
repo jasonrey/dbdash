e726b6dc334e22ca3e354aa45b3eea17dd7a0797
JSON object <jsonobject@outlook.com>
2017-11-30 21:21:00 +0800

    WIP

--- server/routes/static.js
+++ server/routes/static.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const path = require('path')
 
 if (process.env.NODE_ENV !== 'production') {
-  router.use('/dist', express.static(path.resolve(__dirname, '../../dist')))
+  router.use('/', express.static(path.resolve(__dirname, '../../dist')))
   router.use('/docs', express.static(path.resolve(__dirname, '../../docs')))
 }
 
---
+++ src/vue/widgets/empty.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.widget.mb-2
+  h3 Widget Title
+  .types
+    .row
+      .col-3
+        .card
+         .card-body Type 1
+      .col-3
+        .card
+         .card-body Type 1
+      .col-3
+        .card
+         .card-body Type 1
+      .col-3
+        .card
+         .card-body Type 1
+
+</template>
+
+<script>
+export default {
+  name: 'empty-widget',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+
+</style>
