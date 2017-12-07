bf65dfc14cc6180e127e367f2095d93c1d03705e
JSON object <jsonobject@outlook.com>
2017-12-07 17:08:45 +0800

    Add refresh button in tabledata

--- src/vue/widgets/tabledata.vue
+++ src/vue/widgets/tabledata.vue
@@ -23,6 +23,12 @@
   .row.mt-2.mx-0(v-if="!error")
     .col
 
+    button.btn.btn-sm.btn-link.d-flex.align-items-center.mr-3(type="button", @click="loadTable")
+      svg(width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round")
+        polyline(points="23 4 23 10 17 10")
+        polyline(points="1 20 1 14 7 14")
+        path(d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15")
+
     .small.my-auto Total: {{ total }}
 
     .form-group.mx-3.my-0.row
